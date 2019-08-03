bbd7a9d82c53be476531e541d00517f941290b2c
agagnon <alexgagnon227@gmail.com>
2019-08-03 19:54:45 -0400

    Created cards for each section to more easily separate them.

--- client/src/components/_portfolioInfo/introduction.js
+++ client/src/components/_portfolioInfo/introduction.js
@@ -3,19 +3,16 @@ import React from "react";
 
 function introduction(resume) {
     return (
-        <section className="portfolio-section container-fluid">
-            <div>
+        <section className="portfolio-section container-fluid anchor">
+            <div className="content">
                 <h1 id="portfolio-intro-header">
-                    <span>Hi, my name is</span>
+                    <span>Hi, my name is {resume.name}</span>
                 </h1>
-                <h2 key={resume.name} id="portfolio-name">
-                    <span>{resume.name}</span>
+                <h2 id="portfolio-intro-summary m-5">
+                    <span id="portfolio-intro">
+                        I am a {(resume.position && resume.position.toLowerCase())} and I like to break things.
+                    </span>
                 </h2>
-                <h3 id="portfolio-intro-summary m-5">
-                        <span id="portfolio-intro">
-                            I am a {(resume.position && resume.position.toLowerCase())} and I like to break things.
-                        </span>
-                </h3>
             </div>
         </section>
     )
